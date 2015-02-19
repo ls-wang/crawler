ab9a94e9598260ebb62c47caf85e29abe2abf3f6
ls-wang <lswang@LSs-MacBook-Pro.local>
2015-02-19 12:20:51 +0800

    change correct output dir /dev to /data

--- yahootw/runspider.sh
+++ yahootw/runspider.sh
@@ -1,2 +1,2 @@
 # scrapy crawl yahootw -o /data/yahoo1.csv -t csv
-scrapy crawl yahootw --logfile=log/scrapy.log --loglevel=DEBUG -o /dev/yahoo1test.csv -t csv
+scrapy crawl yahootw --logfile=log/scrapy.log --loglevel=DEBUG -o /data/yahoo1test.csv -t csv
